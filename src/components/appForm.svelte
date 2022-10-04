b495e2d8def4b8783d8702d31f7ecad49f60a32d
wjoseph0 <wjoseph0@proton.me>
2022-10-03 21:17:56 -0500

    add form

---
+++ src/components/appForm.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<label>
+  Enter api key:
+  <input type="text" name="api-key" />
+</label>
+
+<label>
+  Enter zipcode:
+  <input type="number" name="zipcode" />
+  <button>Search</button>
+</label>
